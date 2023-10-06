a8e4a427c5385ef025513ca1f6f6bb05f8f9dc8c
kianwasabi <mehrretischgeschmack@gmail.com>
2023-10-06 18:41:20 +0200

    #16 [init] vsomeip server/client (test)

---
+++ src/server/cli.sh
@@ -0,0 +1,2 @@
+/home/seame02/Desktop/commonapi_core_generator/commonapi-core-generator-linux-x86_64 -sk ./fidl/car_control.fidl -d ./src-gen-car_control
+/home/seame02/Desktop/commonapi_someip_generator/commonapi-someip-generator-linux-x86_64  ./fidl/HelloWorld.fdepl -d ./src-gen-car_control
